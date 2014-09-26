fb55fe60fee3db3ff88426eada92fe0142153061
Kevin Chen <kc658@prolog.cs.rutgers.edu>
2014-09-25 22:01:15 -0400

    Start of script to generate clan from config directory

---
+++ src/sprites/generate_clan.py
@@ -0,0 +1,19 @@
+import os
+
+
+"""
+Get all directories
+Directory name should be the prefix of all internal files
+	<dir_name>_<filename>.txt
+	<dir_name>_<subdir_name>
+General file structure:
+	<dir_name>_README.md
+	<dir_name>_pieces.txt
+	<dir_name>_sprites
+		<dir_name>_<piece_type>.png
+"""
+dir_names = [x[0] for x in os.walk(os.path.getcwd())]
+
+
+def make_pieces(dir):
+	pass	
